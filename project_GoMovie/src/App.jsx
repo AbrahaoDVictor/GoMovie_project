45f3b948a4999f943a4d2a78cc926efce9ebed67
Victor Hugo Silva Abrahão De Souza <tri0016912@aluno.firjansenaisesi.com.br>
2026-05-26 21:35:10 -0300

    Cadastra e exibe o filme automático

--- project_GoMovie/src/App.jsx
+++ project_GoMovie/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import MovieForm from './components/MovieForm/MovieForm'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function getMovies() {
+      const response = await fetch("http://localhost:3001/movies")
+      const data = await response.json()
+
+      console.log(data)
+
+      setMovies(data)
+    }
+    getMovies()
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <><div>
+      <h1>Lista de Filmes</h1>
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>Titulo: {movie.title} <br /> Ano: {movie.year} <br /> Diretor: {movie.director} <br /> Gênero: {movie.genre} <br /> Nota: {movie.rating} <br /> Sinopse: {movie.synopsis} <br /> Capa: <img src={movie.cover} alt={movie.title} /></li>
+        ))}
+      </ul>
+    </div><MovieForm handleSubmit={(newMovie) => setMovies((prevMovies) => [...prevMovies, newMovie])} /></>
   )
 }
-
 export default App
